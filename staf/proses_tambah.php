ffc47bb67b4f424dc4f29f3cd7fd0640905b1150
Asep Mulyadi <a.mulyadi@student.upi.edu>
2015-05-02 17:02:55 +0700

    Sudah sudah

--- staf/proses_tambah.php
+++ staf/proses_tambah.php
@@ -7,3 +7,4 @@
 	$dataAdmin = mysql_query("insert into buku values(null, '$_POST[judul]', $_POST[tahun], '$_POST[pengarang]', '$_POST[penerbit]')") or die(mysql_error());
 	header('location:staf-home.php');
 ?>
+
