6b5f4dc6c7b8cdc000ba64db6b042d1179c34615
danangess <dnanxess@gmail.com>
2015-05-02 16:20:06 +0700

    fix delete buku

--- staf/staf-delete_buku.php
+++ staf/staf-delete_buku.php
@@ -1,7 +1,9 @@
 <?php
     $id = $_GET['id_buku'];
     if ($id != "") {
+        include "../conn.php";
         $sql    = "DELETE FROM buku WHERE id = $id";
+        mysql_query ($sql);
     }
     
     header('location:staf-home.php');
