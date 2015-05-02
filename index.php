9063bfef2b4568ccbf3723598f3e6dfea85db5d1
Asep Mulyadi <a.mulyadi@student.upi.edu>
2015-05-02 14:29:28 +0700

    Bootstrap
    Ini bootstrapnya...

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Metode Simplex</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<form class="form-horizontal">
+					<fieldset>
+						<legend>Legend</legend>
+						<div class="form-group">
+							<label for="inputEmail" class="col-lg-2 control-label">Email</label>
+							<div class="col-lg-10">
+								<input type="text" class="form-control" id="inputEmail" placeholder="Email">
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-lg-10 col-lg-offset-2">
+								<button type="submit" class="btn btn-primary">Submit</button>
+							</div>
+						</div>
+					</fieldset>
+				</form>				
+			</div>
+			<div class="col-md-4"></div>
+		</div>
+	</body>
+</html>
